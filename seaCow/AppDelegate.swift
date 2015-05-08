2c8784c272bc32954ae1c63ba50ff2875344e1ad
Liuyuan Tan <tanliuyuan@aliyun.com>
2015-05-07 21:47:50 -0500

    Revert ac73d54..75c7976
    This rolls back to commit ac73d541d6cb66b1ab461463ebfb05aca718a1fb.

--- seaCow/AppDelegate.swift
+++ seaCow/AppDelegate.swift
@@ -19,8 +19,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         Fabric.with([Twitter()])
-        
-        application.registerUserNotificationSettings(UIUserNotificationSettings(forTypes: UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound, categories: nil))
 
         return true
     }
